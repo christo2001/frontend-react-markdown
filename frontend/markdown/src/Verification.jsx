101dc905f1ce9ed7acadf05fedad74fc05f0e836
christo2001 <christolawrencee@gmail.com>
2023-11-29 16:36:22 +0530

    markdown

--- frontend/markdown/src/Verification.jsx
+++ frontend/markdown/src/Verification.jsx
@@ -1,67 +1,44 @@
-// Verification.js
-import React, { useState, useEffect } from 'react';
-import verify from "../images/verified.png"
-import notverify from "../images/notverified.jpg"
-import verifycss from "./verify.module.css"
-import { Link } from 'react-router-dom';
-
+import React, { useEffect, useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import verifycss from './verify.module.css';
 
 const Verification = ({ token }) => {
-  const [message, setMessage] = useState('');
-  const [error, setError] = useState('');
+  const [verificationStatus, setVerificationStatus] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (token) {
-      // Send a GET request to the Express route for user activation
-      fetch(`https://url-shortener-pn6r.onrender.com/api/user/verify/${token}`)
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.message) {
-            setMessage(data.message);
-          } else if (data.error) {
-            setError(data.error);
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-          setError('Internal error');
-        });
-    } else {
-      setError('Token is missing');
-    }
-  }, [token]);
+    const verifyUser = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/api/user/verify/${token}`);
+        const data = await response.json();
+
+        if (response.ok) {
+          setVerificationStatus(data.message || 'User successfully verified!');
+        } else {
+          setVerificationStatus(data.error || 'Verification failed.');
+          navigate('/register');
+        }
+      } catch (error) {
+        console.error('Error during verification:', error);
+        setVerificationStatus('An error occurred during verification.');
+      }
+    };
+
+    verifyUser();
+  }, [token, navigate]);
 
   return (
-    <div className={verifycss.verifybody}>
-
-      {/* success message */}
-  {message && (
     <div className={verifycss.verifybox}>
-      <p className={verifycss.verifymessage}>{message}</p>
-      <img src={verify} className={verifycss.verifyimage}  alt="Verification" />
-      <p className={verifycss.verifyp}> thank you, your email has been verified. your account is now active.</p>
-      <p className={verifycss.verifyp}> please use the link below to login to your account</p>
-      <button className={verifycss.verifybutton}>
-      <Link to="/login" className={verifycss.verifylogin}>Login</Link>
-      </button>
-      
+      {verificationStatus ? (
+        <>
+          <p className={verifycss.verifymessage}>{verificationStatus}</p>
+          {verificationStatus === 'User successfully verified!' && <Link to="/login" className={verifycss.verifybutton}>Go to Login</Link>}
+        </>
+      ) : (
+        <p>Verifying user...</p>
+      )}
     </div>
-  )}
-
-   {/* failure message */}
-  {error &&(
-    <div className={verifycss.verifybox}>
-    <img src={notverify} className={verifycss.verifyimage}  alt="Verification" />
-    <p className={verifycss.verifymessage}>{error}</p>
-    <p className={verifycss.verifyp}> please use the link below to resent the conformation mail to your gmail account</p>
-    <button className={verifycss.verifybutton}>Resend</button>
-    
-  </div>
-  )}
-</div>
-
   );
-  
 };
 
 export default Verification;
